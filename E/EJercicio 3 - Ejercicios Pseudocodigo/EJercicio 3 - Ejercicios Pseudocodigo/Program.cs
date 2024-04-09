770a4083d48e3b5a9ece0aa215ad8fcab0b083c6
Tomascinturion <tomascinturionices@gmail.com>
2024-04-09 18:16:31 -0300

    Ejercicio 3
    agrego ejercicio 3

---
+++ E/EJercicio 3 - Ejercicios Pseudocodigo/EJercicio 3 - Ejercicios Pseudocodigo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+Console.Write("Ingrese el lado: ");
+int lado;
+if (!Int32.TryParse(Console.ReadLine(), out lado))
+{
+    Console.WriteLine("Error un numero");
+}
+else
+{
+    Console.WriteLine("El area del cuadrado es de " + (lado * lado));
+}
