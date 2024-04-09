6994fc478b80dadb16bd9760fc0746bd963dff0a
Tomascinturion <tomascinturionices@gmail.com>
2024-04-09 17:15:29 -0300

    primer commit
    Primer commit donde pegamos los ejercicios de la clase pasada

---
+++ E/Ejercicio 1 - Ejercicios Pseudocodigo/Ejercicio 1 - Ejercicios Pseudocodigo/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+Console.WriteLine("ingrese un num: ");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese un num: ");
+int numero2 = int.Parse(Console.ReadLine());
+int suma = numero1 + numero2;
+Console.WriteLine("El resultado de la suma es:", suma);
+
---
+++ E/Ejercicio 2 - Ejercicios Pseudocodigo/Ejercicio 2 - Ejercicios Pseudocodigo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+Console.WriteLine("Ingrese el numero");
+int numero = int.Parse(Console.ReadLine());
+if (numero % 2 == 0) 
+    {
+      Console.WriteLine("Es Par");
+    }
+else
+{
+    Console.WriteLine("Es Impar");
+}
