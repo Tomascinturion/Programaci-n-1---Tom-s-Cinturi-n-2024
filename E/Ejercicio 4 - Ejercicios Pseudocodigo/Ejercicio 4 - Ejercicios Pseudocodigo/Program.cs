9c564024f5742a1eeed118a00b26fcff66950594
Tomascinturion <tomascinturionices@gmail.com>
2024-04-10 19:33:47 -0300

    Ejercicio 4

---
+++ E/Ejercicio 4 - Ejercicios Pseudocodigo/Ejercicio 4 - Ejercicios Pseudocodigo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Calcular el promedio de tres números.
+
+Console.Write("Ingrese un numero: ");
+float num1, num2, num3, prom;
+if (!float.TryParse(Console.ReadLine(), out num1))
+{
+    Console.WriteLine("Error, ingrese un numero");
+}
+
+else
+{
+    Console.Write("Ingrese el segunto numero: ");
+    if(!float.TryParse(Console.ReadLine(),out num2))
+    {
+        Console.WriteLine("Error, ingrese un numero");
+    }
+    else
+    {
+        Console.Write("Ingrese el tercer numero: ");
+        if(!float.TryParse(Console.ReadLine(),out num3))
+        {
+            Console.WriteLine("Error, ingrese un numero");
+        }
+        else
+        {
+            prom = (num1 + num2 + num3) / 3;
+            Console.WriteLine("El promedio es de: " +  prom);
+        }
+    }
+}
+
